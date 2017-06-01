93545fb83e0ee1b02c7d50d71a8db00cea913405
Kaitaku Takeda <katakeda@ucsc.edu>
2017-05-31 23:53:04 -0700

    Almost done

--- js/main.js
+++ js/main.js
@@ -1,3 +1,38 @@
+window.onload = function() {
+  $('#intro-title').textillate({
+    initialDelay: 1000,
+    in: {
+      effect: 'fadeIn',
+      delay: 200,
+      shuffle: true
+    }
+  });
+  $('#intro-text').textillate({
+    initialDelay: 300,
+    in: {
+      effect: 'fadeIn',
+      delay: 10,
+      shuffle: true
+    }
+  });
+  if($(this).scrollTop()) {
+    $('.nav-text').textillate({
+      in: {
+        effect: 'fadeIn',
+        sync: true
+      }
+    });
+  } else {
+    $('.nav-text').textillate({
+      initialDelay: 2800,
+      in: {
+        effect: 'fadeIn',
+        sync: true
+      }
+    });
+  }
+}
+
 window.onscroll = function() {
   var st = $(this).scrollTop();
   var min = $(this).innerHeight()-$('#my-navbar').height();
@@ -6,6 +41,18 @@ window.onscroll = function() {
   } else {
     $('#my-navbar').removeClass('onScroll-navbar');
   }
+}
+
+function goToAbout() {
+  $('html,body').animate({
+    scrollTop: $("#first-container").offset().top
+  }, 'slow');
+}
+
+function goToProjects() {
+  $('html,body').animate({
+    scrollTop: $("#second-container").offset().top
+  }, 'slow');
 }
 
 function goToGithub() {
